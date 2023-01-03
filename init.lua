72ca69a6c331246cd059321493357caa3496f4c7
Ravi Sinha <ravisinha@my.unt.edu>
2023-01-02 22:37:55 -0600

    Add quick line movement bindings

--- init.lua
+++ init.lua
@@ -144,6 +144,9 @@ require('gruvbox').setup({
 })
 vim.cmd [[colorscheme gruvbox]]
 
+-- Set default GUI font
+vim.cmd [[set guifont="Source Code Pro:h18"]]
+
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
@@ -238,6 +241,10 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+-- Quickly move lines up and down
+vim.keymap.set('n', '-', 'ddp')
+vim.keymap.set('n', '_', 'ddkP')
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
