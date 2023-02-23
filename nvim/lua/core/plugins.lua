35f614a31404a40bc47de16b600fce5ced450de1
Ravi Sinha <ravisinha@my.unt.edu>
2023-02-22 18:51:18 -0600

    Add WhichKey to Neovim

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -34,6 +34,19 @@ require('packer').startup(function(use)
 
   use "rafamadriz/friendly-snippets"
 
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
   use { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     run = function()
