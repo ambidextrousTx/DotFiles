22e46e40ba482fca7ff6147359b5e9bd86f93fa9
Ravi Sinha <ravisinha@my.unt.edu>
2023-05-10 22:57:37 -0500

    Reflect Treesitter help name change from help to vimdoc

--- nvim/lua/core/plugin_config/treesitter.lua
+++ nvim/lua/core/plugin_config/treesitter.lua
@@ -2,7 +2,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'javascript', 'help', 'vim', 'php', 'clojure' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'javascript', 'vimdoc', 'vim', 'php', 'clojure' },
 
   sync_install = false,
   auto_install = true,
