3d50c8f42dda26ad70fe265a964aef6b39b65861
Ravi Sinha <ravisinha@my.unt.edu>
2023-06-20 19:29:33 -0500

    Python debugging: first draft

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -77,7 +77,7 @@ require('mason').setup()
 local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
+  ensure_installed = vim.tbl_keys(servers)
 }
 
 mason_lspconfig.setup_handlers {
--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -98,6 +98,38 @@ require('packer').startup(function(use)
       end
   }
 
+  -- DAP for debugging Python
+  use { 'rcarriga/nvim-dap-ui',
+    dependencies = 'mfussenegger/nvim-dap',
+    config = function ()
+      local dap = require('dap')
+      local dapui = require('dapui')
+      dapui.setup()
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+      end
+    end
+  }
+  use { 'mfussenegger/nvim-dap' }
+  use {
+    'mfussenegger/nvim-dap-python',
+    ft = 'python',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+      'rcarriga/nvim-dap-ui'
+    },
+    config = function(_, opts)
+      local path = '~/.local/share/nvim/mason/packages/debugpy/venv/bin/python'
+      require('dap-python').setup(path)
+    end,
+  }
+
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
   if has_plugins then
