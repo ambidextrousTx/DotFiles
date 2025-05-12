547d3e9ec8b0aeaa5e8dcb09e0d6bff09da95bf3
Ravi Sinha <ravisinha@my.unt.edu>
2025-05-11 21:23:28 -0500

    Migrate from Packer to Lazy because Lazy rocks

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,27 +1,31 @@
--- Install packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
-  vim.cmd [[packadd packer.nvim]]
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
+vim.opt.rtp:prepend(lazypath)
 
-require('packer').startup(function(use)
-  -- Package manager
-  use 'wbthomason/packer.nvim'
-
-  use {
+local plugins = {
+  {
     'j-hui/fidget.nvim',
     tag = 'legacy'
-  }
-
-  use { -- LSP Configuration & Plugins
+  },
+  { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
-    requires = {
+    dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
+      {'mason-org/mason.nvim', version = '^1.0.0'},
+      {'mason-org/mason-lspconfig.nvim', version = '^1.0.0'},
 
       -- Useful status updates for LSP
       'j-hui/fidget.nvim',
@@ -29,17 +33,17 @@ require('packer').startup(function(use)
       -- Additional lua configuration, makes nvim stuff amazing
       'folke/neodev.nvim',
     },
-  }
+  },
 
   -- Autocompletion
-  use {
+  {
     'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  }
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+  },
 
-  use "rafamadriz/friendly-snippets"
+  'rafamadriz/friendly-snippets',
 
-  use {
+  {
     -- "folke/which-key.nvim",
     "max397574/which-key.nvim",
     config = function()
@@ -51,69 +55,69 @@ require('packer').startup(function(use)
         -- refer to the configuration section below
       }
     end
-  }
+  },
 
-  use { -- Highlight, edit, and navigate code
+  { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     run = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
-  }
+  },
 
-  use { -- Additional text objects via treesitter
+  { -- Additional text objects via treesitter
     'nvim-treesitter/nvim-treesitter-textobjects',
     after = 'nvim-treesitter',
-  }
+  },
 
-  use 'nvim-tree/nvim-tree.lua' -- Directory structure
-  use 'nvim-tree/nvim-web-devicons' -- Lovely icons
+  'nvim-tree/nvim-tree.lua', -- Directory structure
+  'nvim-tree/nvim-web-devicons', -- Lovely icons
 
   -- Git related plugins
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-rhubarb'
-  use 'lewis6991/gitsigns.nvim'
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+  'lewis6991/gitsigns.nvim',
 
   -- Themes
-  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
-  use 'ellisonleao/gruvbox.nvim' -- Neovim port of Gruvbox
-  use 'rose-pine/neovim' -- Rose-pine theme
-  use "EdenEast/nightfox.nvim" -- The Nightbox set of themes
-  use "folke/tokyonight.nvim" -- The tokyonight set of themes
-  use { 'catppuccin/nvim', as = 'catppuccin' } -- Catppuccin colors
-  use 'marko-cerovac/material.nvim' -- Several material themes
-  use 'RRethy/nvim-base16' -- Several base-16 themes, including Atelier Dune
+  'navarasu/onedark.nvim', -- Theme inspired by Atom
+  'ellisonleao/gruvbox.nvim', -- Neovim port of Gruvbox
+  'rose-pine/neovim', -- Rose-pine theme
+  'EdenEast/nightfox.nvim', -- The Nightbox set of themes
+  'folke/tokyonight.nvim', -- The tokyonight set of themes
+  { 'catppuccin/nvim', as = 'catppuccin' }, -- Catppuccin colors
+  'RRethy/nvim-base16', -- Several base-16 themes, including Atelier Dune
+  'marko-cerovac/material.nvim', -- Material themes
 
-  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-  use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+  'nvim-lualine/lualine.nvim', -- Fancier statusline
+  'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
+  'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- Fuzzy Finder (files, lsp, etc)
-  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
+  { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 },
 
   -- Harpoon to quickly switch between chosen files
-  use { 'theprimeagen/harpoon' }
+  'theprimeagen/harpoon',
 
   -- Undotree to graphically visualize changes
-  use { 'mbbill/undotree' }
+  'mbbill/undotree',
 
   -- PlantUML syntax highlighting
-  use 'javiorfo/nvim-nyctophilia'
+  'javiorfo/nvim-nyctophilia',
 
   -- Autopairs to automatically close [], {}, (), "", ''
-  use { 'windwp/nvim-autopairs',
+  { 'windwp/nvim-autopairs',
       config = function()
         require('nvim-autopairs').setup {}
       end
-  }
+  },
 
-  use { 'nvim-neotest/nvim-nio' }
+  'nvim-neotest/nvim-nio',
 
   -- DAP for debugging Python
-  use { 'rcarriga/nvim-dap-ui',
+  { 'rcarriga/nvim-dap-ui',
     dependencies = 'mfussenegger/nvim-dap',
     config = function ()
       local dap = require('dap')
@@ -129,9 +133,9 @@ require('packer').startup(function(use)
         dapui.close()
       end
     end
-  }
-  use { 'mfussenegger/nvim-dap' }
-  use {
+  },
+  'mfussenegger/nvim-dap',
+  {
     'mfussenegger/nvim-dap-python',
     ft = 'python',
     dependencies = {
@@ -143,28 +147,8 @@ require('packer').startup(function(use)
       require('dap-python').setup(path)
     end,
   }
+}
 
-  -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
-  local has_plugins, plugins = pcall(require, 'custom.plugins')
-  if has_plugins then
-    plugins(use)
-  end
+local opts = {}
 
-  if is_bootstrap then
-    require('packer').sync()
-  end
-end)
-
--- When we are bootstrapping a configuration, it doesn't
--- make sense to execute the rest of the init.lua.
---
--- You'll need to restart nvim, and then it will work.
-if is_bootstrap then
-  print '=================================='
-  print '    Plugins are being installed'
-  print '    Wait until Packer completes,'
-  print '       then restart nvim'
-  print '=================================='
-  return
-end
-
+require('lazy').setup(plugins, opts)
